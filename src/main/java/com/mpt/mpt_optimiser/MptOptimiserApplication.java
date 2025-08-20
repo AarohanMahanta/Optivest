a1e003680604296a7480bb4b5c6d60ea5ba4f3af
aarohanmahanta <go57fim@mytum.de>
2025-08-20 16:03:41 +0530

    Initial Project Structure

---
+++ src/main/java/com/mpt/mpt_optimiser/MptOptimiserApplication.java
@@ -0,0 +1,13 @@
+package com.mpt.mpt_optimiser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MptOptimiserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MptOptimiserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mpt/mpt_optimiser/MptOptimiserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpt.mpt_optimiser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MptOptimiserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
