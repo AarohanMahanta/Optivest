28ffab9936afb1fba895690e983b0961c67d42ba
AarohanMahanta <aarohanmahanta@gmail.com>
2025-08-21 17:09:39 +0530

    test: add test for AssetController GET endpoints using EasyMock and MockMVC

--- src/test/java/com/mpt/mpt_optimiser/MptOptimiserApplicationTests.java
+++ src/test/java/com/mpt/mpt_optimiser/MptOptimiserApplicationTests.java
@@ -1,13 +1,58 @@
 package com.mpt.mpt_optimiser;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 
+import com.mpt.mpt_optimiser.controller.AssetController;
+import com.mpt.mpt_optimiser.dao.AssetDAO;
+import com.mpt.mpt_optimiser.model.Asset;
+import com.mpt.mpt_optimiser.service.AssetService;
+import com.mpt.mpt_optimiser.service.PortfolioService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.easymock.EasyMock;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.easymock.EasyMock.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class MptOptimiserApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    private AssetService assetService;
+    private PortfolioService portfolioService;
+    private AssetController assetController;
+    private MockMvc mockMvc;
+
+    @BeforeEach
+	void setUp() {
+		assetService = createMock(AssetService.class);
+        portfolioService = createMock(PortfolioService.class);
+        assetController = new AssetController(assetService, portfolioService);
+        mockMvc = MockMvcBuilders.standaloneSetup(assetController).build();
+    }
+
+    @Test
+    void testGetAllAssets() throws Exception {
+        Asset asset1 = new Asset(0.1, 1L, "AAPL", 0.2);
+        Asset asset2 = new Asset(0.15, 2L, "PLTR", 0.25);
+
+        expect(assetService.getAllAssets()).andReturn(Arrays.asList(asset1, asset2));
+        replay(assetService);
+
+        mockMvc.perform(get("/api/assets"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[*].ticker").value(containsInAnyOrder("AAPL", "PLTR")));
+        verify(assetService);
+    }
+
 
 }
