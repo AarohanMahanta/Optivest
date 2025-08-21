f16c692114a49dd2aca930420863a3ba7cf14d78
AarohanMahanta <aarohanmahanta@gmail.com>
2025-08-21 11:42:45 +0530

    feat: implement retrieval of ticker prices using yfinance api

--- python-service/app.py
+++ python-service/app.py
@@ -1,4 +1,7 @@
 from flask import Flask, request, jsonify
+import yfinance as yf
+
+
 app = Flask(__name__)
 
 @app.route('/optimise', methods=['POST'])
@@ -8,3 +11,7 @@ def optimise():
 
     if not tickers:
         return jsonify({"error" : "No tickers provided"}), 400
+
+    #retrieving prices from yfinance
+    prices = yf.download(tickers, period="1y")['Adj Close'] #Adjusted Close prices; better suited for return calculations compared to raw close prices
+
